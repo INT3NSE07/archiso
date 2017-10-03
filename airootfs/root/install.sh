8517790aa7bb1e7e3dde3ff6b38b23523a769acf
INT3NSE07 <d.jonathanpaul@gmail.com>
2017-10-03 23:19:31 +0530

    arch: Increase cowspace for git

--- airootfs/root/install.sh
+++ airootfs/root/install.sh
@@ -8,6 +8,9 @@ do
    ping -c1 www.google.com &> /dev/null && break
 done
 
+# Increase cowspace
+mount -o remount,size=2G /run/archiso/cowspace
+
 git clone https://bitbucket.org/INT3NSE07/aui/ arch-scripts
 cd arch-scripts
 ./install.sh
--- airootfs/root/post-install.sh
+++ airootfs/root/post-install.sh
@@ -8,5 +8,8 @@ do
    ping -c1 www.google.com &> /dev/null && break
 done
 
+# Remount
+mount -o remount,size=2G /run/archiso/cowspace
+
 cd arch-scripts/
 ./post-install.sh
