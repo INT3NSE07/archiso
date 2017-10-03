4452e401e73cda724bb5926ca5ace4c16d6e35ca
INT3NSE07 <d.jonathanpaul@gmail.com>
2017-10-03 20:22:27 +0530

    arch: Add arch scripts

--- airootfs/root/autorun.sh
+++ airootfs/root/autorun.sh
@@ -1,3 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-echo "Hello world"
+echo "Arch-Linux setup"
+echo "Waiting for network connection... "
+
+while true
+do
+   ping -c1 www.google.com &> /dev/null && break
+done
+
+git clone https://bitbucket.org/INT3NSE07/aui/ arch-scripts
+./arch-scripts/install.sh
+
