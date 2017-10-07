e00ed698d67fe9264797d746060810c8c46b131a
INT3NSE07 <d.jonathanpaul@gmail.com>
2017-10-07 18:56:25 +0530

    arch: Update path variable name

--- db.sh
+++ db.sh
@@ -2,11 +2,11 @@
 
 ARCH='x86_64'
 MIRROR='https://mirrors.kernel.org/archlinux/'
-PATH=pacmandb
+DIRPATH=pacmandb
 
-wget -P ${PATH} "${MIRROR}/community/os/${ARCH}/community.db"
-wget -P ${PATH} "${MIRROR}/core/os/${ARCH}/core.db"
-wget -P ${PATH} "${MIRROR}/extra/os/${ARCH}/extra.db"
+wget -P ${DIRPATH} "${MIRROR}/community/os/${ARCH}/community.db"
+wget -P ${DIRPATH} "${MIRROR}/core/os/${ARCH}/core.db"
+wget -P ${DIRPATH} "${MIRROR}/extra/os/${ARCH}/extra.db"
 if [ "$ARCH" == "x86_64" ]; then
-  wget -P ${PATH} "${MIRROR}/multilib/os/${ARCH}/multilib.db"
+  wget -P ${DIRPATH} "${MIRROR}/multilib/os/${ARCH}/multilib.db"
 fi
