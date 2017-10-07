2651d4c3a2041198a0c27606b8d3bb4d902f3dd9
INT3NSE07 <d.jonathanpaul@gmail.com>
2017-10-07 08:40:24 +0530

    arch: Move checkconnection to sharedfuncs

--- airootfs/root/install.sh
+++ airootfs/root/install.sh
@@ -2,10 +2,6 @@
 
 echo "Arch-Linux installation script"
 
-unset NETC
-ping -c1 www.google.com &> /dev/null
-NETC=$?
-
 # Increase cowspace
 mount -o remount,size=4G /run/archiso/cowspace
 
--- airootfs/root/post-install.sh
+++ airootfs/root/post-install.sh
@@ -2,10 +2,6 @@
 
 echo "Arch-Linux post installation script"
 
-unset NETC
-ping -c1 www.google.com &> /dev/null
-NETC=$?
-
 # Remount
 mount -o remount,size=4G /run/archiso/cowspace
 
